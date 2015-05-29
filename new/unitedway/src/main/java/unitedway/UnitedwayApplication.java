6fc5a74e7f414f39e89308711479c03024800939
David Wong <david492000@gmail.com>
2015-05-29 10:51:20 -0400

    init

---
+++ new/unitedway/src/main/java/unitedway/UnitedwayApplication.java
@@ -0,0 +1,12 @@
+package unitedway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitedwayApplication.class, args);
+    }
+}
